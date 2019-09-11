4813584d1dd6ec514a5bb0a8dfc835c1bcd16586
Daniel Grychtol <grychtol.daniel@gmail.com>
2019-09-11 09:28:24 +0200

    Clear

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,54 +1,22 @@
 import React, { Component, Fragment } from 'react';
 import { hot } from 'react-hot-loader';
-import uuid from 'uuid';
 
-// Components
-import Title from '../Components/Title';
-import ToDoForm from '../Components/ToDoForm';
-import ToDos from '../Components/ToDos';
+// STYLES
+import style from '../styles/main.scss';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      todos: [],
-      error: false
+      state: 'test'
     };
   }
 
-  addTodo = nameOfTodo => {
-    this.setState({ error: false });
-
-    if (nameOfTodo && nameOfTodo.trim().length > 0) {
-      const newTodoItem = {
-        name: nameOfTodo.trim(),
-        id: uuid.v4()
-      };
-      const todos = [...this.state.todos, newTodoItem];
-
-      this.setState({ todos });
-    } else {
-      this.setState({ error: true });
-    }
-  };
-
-  removeTodo = id => {
-    const remainingTodos = this.state.todos.filter(item => item.id !== id);
-
-    this.setState({
-      todos: remainingTodos
-    });
-  };
-
   render() {
-    const { todos } = this.state;
-
     return (
       <Fragment>
-        <Title />
-        <ToDoForm addTodo={this.addTodo} />
-        {this.state.error && <small>type in the name!</small>}
-        <ToDos todos={todos} removeTodo={this.removeTodo} />
+        <h2 className={style.main}>Custom react boilerplate</h2>
+        <p>test</p>
       </Fragment>
     );
   }
