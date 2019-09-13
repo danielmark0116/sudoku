e822fdd8d5b4eb26129229fca35142f19ff12414
Daniel Grychtol <grychtol.daniel@gmail.com>
2019-09-13 15:57:32 +0200

    Add btn styles and btn animations

--- src/Containers/Menu.js
+++ src/Containers/Menu.js
@@ -1,6 +1,22 @@
 import React, { Component, Fragment } from 'react';
 
+import style from '../styles/main.scss';
+
+import { btnPush, resetBtn, btnUp } from '../animation/btn';
+
 class Menu extends Component {
+  animateOnPush = node => {
+    btnPush(node);
+  };
+
+  cancelBtnClick = node => {
+    resetBtn(node);
+  };
+
+  animateOnUp = node => {
+    btnUp(node);
+  };
+
   render() {
     const {
       solve,
@@ -14,13 +30,111 @@ class Menu extends Component {
 
     return (
       <Fragment>
-        <button onClick={solve}>solve</button>
-        <button onClick={generate}>generate</button>
-        <button onClick={check}>check</button>
-        <button onClick={showEmptyIndexes}>show empty indexes</button>
-        <button onClick={printState}>print state</button>
-        <button onClick={save}>save</button>
-        <button onClick={load}>load</button>
+        <button
+          className={`${style.small} ${style.custom_btn}`}
+          onClick={solve}
+          onMouseDown={e => {
+            this.animateOnPush(e.target);
+          }}
+          onMouseUp={e => {
+            this.animateOnUp(e.target);
+          }}
+          onMouseLeave={e => {
+            this.cancelBtnClick(e.target);
+          }}
+        >
+          solve
+        </button>
+        <button
+          className={`${style.small} ${style.custom_btn}`}
+          onClick={generate}
+          onMouseDown={e => {
+            this.animateOnPush(e.target);
+          }}
+          onMouseUp={e => {
+            this.animateOnUp(e.target);
+          }}
+          onMouseLeave={e => {
+            this.cancelBtnClick(e.target);
+          }}
+        >
+          generate
+        </button>
+        <button
+          className={`${style.small} ${style.custom_btn}`}
+          onClick={check}
+          onMouseDown={e => {
+            this.animateOnPush(e.target);
+          }}
+          onMouseUp={e => {
+            this.animateOnUp(e.target);
+          }}
+          onMouseLeave={e => {
+            this.cancelBtnClick(e.target);
+          }}
+        >
+          check
+        </button>
+        <button
+          className={`${style.small} ${style.custom_btn}`}
+          onClick={showEmptyIndexes}
+          onMouseDown={e => {
+            this.animateOnPush(e.target);
+          }}
+          onMouseUp={e => {
+            this.animateOnUp(e.target);
+          }}
+          onMouseLeave={e => {
+            this.cancelBtnClick(e.target);
+          }}
+        >
+          show empty indexes
+        </button>
+        <button
+          className={`${style.small} ${style.custom_btn}`}
+          onClick={printState}
+          onMouseDown={e => {
+            this.animateOnPush(e.target);
+          }}
+          onMouseUp={e => {
+            this.animateOnUp(e.target);
+          }}
+          onMouseLeave={e => {
+            this.cancelBtnClick(e.target);
+          }}
+        >
+          print state
+        </button>
+        <button
+          className={`${style.small} ${style.custom_btn}`}
+          onClick={save}
+          onMouseDown={e => {
+            this.animateOnPush(e.target);
+          }}
+          onMouseUp={e => {
+            this.animateOnUp(e.target);
+          }}
+          onMouseLeave={e => {
+            this.cancelBtnClick(e.target);
+          }}
+        >
+          save
+        </button>
+        <button
+          className={`${style.small} ${style.custom_btn}`}
+          onClick={load}
+          onMouseDown={e => {
+            this.animateOnPush(e.target);
+          }}
+          onMouseUp={e => {
+            this.animateOnUp(e.target);
+          }}
+          onMouseLeave={e => {
+            this.cancelBtnClick(e.target);
+          }}
+        >
+          load
+        </button>
       </Fragment>
     );
   }
---
+++ src/animation/btn.js
@@ -0,0 +1,34 @@
+import {
+  TweenMax,
+  TimelineLite,
+  Power2,
+  Elastic,
+  CSSPlugin,
+  Bounce,
+  Power1
+} from 'gsap/TweenMax';
+
+const t1 = new TimelineLite();
+
+const easing = Back.easeOut.config(4);
+
+export const btnPush = node => {
+  t1.to(node, 0.2, { scale: 0.9, ease: easing });
+};
+
+export const resetBtn = node => {
+  TweenMax.to(node, 0.7, { scale: 1, ease: easing });
+};
+
+export const btnUp = node => {
+  if (!t1._active) {
+    t1.to(node, 0.05, {
+      scale: 1,
+      rotation: 0
+    })
+      .to(node, 0.05, { scale: 1.3, rotation: 15 })
+      .to(node, 1, { scale: 1, rotation: 0, ease: easing });
+  } else {
+    TweenMax.to(node, 0.2, { scale: 1, ease: Power1.easeIn });
+  }
+};
