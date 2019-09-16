6e906e8550ac990523c19209f024dbcef6e756d3
Daniel Grychtol <grychtol.daniel@gmail.com>
2019-09-16 02:05:59 +0200

    Update

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,7 @@ module.exports = env => {
     entry: './src/index.js',
     optimization: optimizationObject,
     output: {
-      path: path.resolve(__dirname, 'docs'),
+      path: path.resolve(__dirname, 'build'),
       filename: 'app.bundle.js'
     },
     devServer: { port: 3000 },
